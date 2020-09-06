0ad52a28fef4a71388f69aeea02db4bc64d8da9f
BUS410 <wirtzartyom410@gmail.com>
2020-09-06 12:24:21 +0600

    Add redirect to icon.

--- Tests/urls.py
+++ Tests/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico',
+         permanent=True)),
 ]
